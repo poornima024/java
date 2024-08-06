3db4bad18af9df87447993c3f6b4b2d3cf94d835
Poornima shrotriya <127727928+poornima024@users.noreply.github.com>
2024-08-06 15:34:17 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,23 @@
+package Wrapper_class;
+import java.util.ArrayList;
+
+
+class  Autoboxing {
+
+       public static void main(String[] args) {
+           
+             char ch = 'a';
+
+             Character a = ch;
+
+             ArrayList <Integer> arr = new ArrayList<>();
+
+             arr.add(5);
+             arr.add(10);
+
+             System.out.println(arr.get(1));
+
+
+       }
+    
+}
