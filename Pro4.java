dee9ad26f2398ad76f6f4bfffa7add9669b5493c
Poornima shrotriya <127727928+poornima024@users.noreply.github.com>
2024-09-03 22:44:59 +0530

    Add files via upload

---
+++ Pro4.java
@@ -0,0 +1,17 @@
+public class Pro4 {
+    
+      public static void main(String[] args) {
+           
+             String Str1 = "example.com";
+             String Str2 = "example.com";
+
+             if(Str1.equals(Str2))
+             {
+                     System.out.println("True");
+             }
+             else
+             {
+                         System.out.println("False");
+             }
+      }
+}
